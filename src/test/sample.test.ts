395e9e05dd19c61c40ab4a98c19d2b02206b46ad
John Luke Rodrigo Pana <panajohnluke@gmail.com>
2026-04-23 15:50:42 +0000

    sample tests

---
+++ src/test/sample.test.ts
@@ -0,0 +1,17 @@
+describe("Basic Tests", () => {
+
+  test("Addition works", () => {
+    expect(2 + 3).toBe(5);
+  });
+
+  test("Subtraction works", () => {
+    expect(5 - 2).toBe(3);
+  });
+
+  test("Multiplication works", () => {
+    expect(3 * 3).toBe(9);
+  });
+
+  test("Division works", () => {
+    expect(10 / 2).toBe(5);
+  });
